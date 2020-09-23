8f0367d6b11c0885c0c85c0d4268e6992c22e434
witjs <github@sina.cn>
2020-09-23 17:23:48 +0800

    feat: eslint for test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,18 @@
 module.exports = {
   root: true,
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: "./",
+  },
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/no-namespace": "off",
   },
 };
-// module.exports = {
-//   settings: {
-//     "import/resolver": {
-//       typescript: {},
-//     },
-//   },
-//   parser: "@typescript-eslint/parser",
-//   parserOptions: {
-//     project: "./tsconfig.json",
-//     tsconfigRootDir: "./",
-//   },
-//   plugins: ["@typescript-eslint", "import"],
-//   extends: [
-//     "plugin:@typescript-eslint/recommended",
-//     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-//   ],
-//   rules: {
-//     "@typescript-eslint/no-namespace": "off",
-//   },
-// };
